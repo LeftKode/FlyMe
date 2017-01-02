eaf1bec37c12c9c32d4c72cac9223ecc61554426
tolisich <it14126@uom.edu.gr>
2017-01-02 16:36:50 +0200

    Προσθήκη χρωμάτων στα layout

--- app/src/main/java/com/mobiledev/uom/flyme/ResultsActivityFragment.java
+++ app/src/main/java/com/mobiledev/uom/flyme/ResultsActivityFragment.java
@@ -121,7 +121,6 @@ public class ResultsActivityFragment extends Fragment {
             retunDateString = data.getString(data.getColumnIndexOrThrow("returnDate"));
             nonStopValue =  data.getInt(data.getColumnIndexOrThrow("nonStop"));
 
-
             ShowFlightsTask flightsTask = new ShowFlightsTask();
 
             if(isOnline()){
